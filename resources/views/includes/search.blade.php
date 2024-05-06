fdbaac5c3e05b9f5895c2e7b0a37cc60c9c4a77c
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-05-06 17:06:58 +0800

    search & chart button added

---
+++ resources/views/includes/search.blade.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-7">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search name" aria-label="Recipient's username" aria-describedby="search-button" id="search-input">
+                <button class="btn maroon-button" type="button" id="search-button"><i class="fa fa-search color-white"></i></button>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="row">
+                <div class="col text-center">
+                    <div class="row mt-2">
+                        <div class="col">
+                            <strong>Show Status Statistic</strong>
+                        </div>
+                        <div class="col-2">
+                            <input class="form-check-input" type="checkbox" value="" aria-label="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col text-center">
+                    <div class="row mt-2">
+                        <div class="col">
+                            <strong>Show Rank Statistic</strong>
+                        </div>
+                        <div class="col col-2">
+                            <input class="form-check-input" type="checkbox" value="" aria-label="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,7 @@
 @section('title', 'MSU FACULTY DIRECTORY')
 
 @section('content')
-
+<div class="container border mt-3">
+    <h1>Hello World</h1>
+</div>
 @endsection

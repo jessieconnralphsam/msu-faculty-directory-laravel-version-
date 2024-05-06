b548ca803278ed2898b92bba78c0baa1143b2798
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-05-06 20:47:32 +0800

    footer added

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+      <div class="footer-bg">
+        <div class="footer-container">
+          <div class="container-foot d-flex justify-content-center">
+            <p class="color-white text-center mt-2">Copyright © 2024. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+</footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,7 @@
     @include('includes.navigation')
     @include('includes.search')
     @yield('content')
+    @include('includes.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="{{ asset('js/main.js') }}"></script>
 </body>

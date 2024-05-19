5bbce0429964a95bbca33ddd9737c5a271b2f0cd
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-05-19 09:50:04 +0800

    collegeController [commit added]

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -8,13 +8,30 @@ use App\Services\CollegeService;
 
 class CollegeController extends Controller
 {
+    /**
+     * The faculty service instance.
+     */
     protected $facultyService;
+
+     /**
+     * Create a new controller instance.
+     *
+     * @param CollegeService $facultyService
+     */
 
     public function __construct(CollegeService $facultyService) 
     {
+         // injected service to the protected property
         $this->facultyService = $facultyService;
     }
 
+    /**
+     * Display the specified college along with its faculty members.
+     *
+     * @param int $collegeId
+     * @return \Illuminate\View\View
+     */
+    
     public function show($collegeId)
     {
         $college = College::with('faculty')->findOrFail($collegeId);

c27c1a0b72f3230eb69ffde107248fe07bc289b0
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-05-19 10:29:11 +0800

    update ProfielController [add comment]

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,14 +9,29 @@ class ProfileController extends Controller
 {
     protected $profileService;
 
+    /**
+     * Create a new controller instance.
+     *
+     * @param ProfileService $profileService The profile service instance.
+     */
     public function __construct(ProfileService $profileService)
     {
         $this->profileService = $profileService;
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param int $profileId The ID of the profile to show.
+     * @return \Illuminate\Http\Response The profile view.
+     */
+    
     public function show($profileId)
     {
+        // Get profile data using ProfileService
         $data = $this->profileService->getProfileData($profileId);
+
+        // Return the profile view with data
         return view('profile', $data);
     }
 }
